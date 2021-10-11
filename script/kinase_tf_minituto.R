d8650b3c3c6a1d3ec99b76aa8426545431bf370b
adugourd <aurelien.dugourd@bioquant.uni-heidelberg.de>
2021-10-11 16:21:20 +0200

    updated names for new decoupler

--- script/kinase_tf_minituto.R
+++ script/kinase_tf_minituto.R
@@ -33,9 +33,11 @@ RNA_differential_analysis <- unique(RNA_differential_analysis)
 row.names(RNA_differential_analysis) <- RNA_differential_analysis$ID
 RNA_differential_analysis <- RNA_differential_analysis[,"t",drop = F]
 
+dorothea_df <- intersect_regulons(as.matrix(RNA_differential_analysis), dorothea_df, .source = tf, .target = target, minsize=20)
+
 #Now we estimate the TF activities using run_mean from decoupleR
-TF_activities <- as.data.frame(run_mean(mat = as.matrix(RNA_differential_analysis), network = dorothea_df, times = 1000))
-TF_activities <- TF_activities[TF_activities$statistic == "normalized_mean",c(2,4,5)]
+TF_activities <- as.data.frame(run_wmean(mat = as.matrix(RNA_differential_analysis), network = dorothea_df, .source = "tf", .target = "target", times = 1000))
+TF_activities <- TF_activities[TF_activities$statistic == "norm_wmean",c(2,4,5)]
 
 ########## PHOSHO and KINASE part ########
 
@@ -63,13 +65,15 @@ KSN <- KSN[!duplicated(KSN$id),]
 KSN <- KSN[,-5]
 
 #rename KSN to fit decoupler format
-names(KSN)[c(1,2)] <- c("target","tf")
+names(KSN)[c(1,2)] <- c("phospho_site","kinase")
+
+KSN <- intersect_regulons(as.matrix(phospho_differential_analysis), KSN, .source = kinase, .target = phospho_site, minsize=5)
 
 #using run_mean from decoupleR to get the TF activities from the phosphoproteomic data
 #You can also run that on wour normalised intensity matrix of phosphosites directly,
 #as long as it is formatted as a dataframe of similar format as here
-kin_activity <- run_mean(mat = as.matrix(phospho_differential_analysis),network = KSN, times = 1000)
-kin_activity <- kin_activity[kin_activity$statistic == "normalized_mean",c(2,4,5)]
+kin_activity <- run_wmean(mat = as.matrix(phospho_differential_analysis), .source = "kinase", .target = "phospho_site",network = KSN, times = 1000)
+kin_activity <- kin_activity[kin_activity$statistic == "norm_wmean",c(2,4,5)]
 
 ########## CONCLUSION
 
